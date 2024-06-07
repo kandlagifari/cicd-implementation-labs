9fa15215467d79a06417d3b2147a00f32ea359e8
kandlagifari <kandlagifari@gmail.com>
2024-06-07 16:04:52 +0700

    Rename directory

--- terraform/environment/01_trivia-app/provider.tf
+++ terraform/environment/01_trivia-app/provider.tf
@@ -28,7 +28,7 @@ provider "aws" {
   profile             = var.aws_profile
   default_tags {
     tags = {
-      Environment = "prod"
+      Environment = "trivia-app"
       Project     = "cicd"
     }
   }
